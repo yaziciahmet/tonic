f7d58b9be9384eb319e31d15d0651c9cb338fd0d
yaziciahmet <yaziciahmet991@gmail.com>
2024-10-24 09:01:42 +0200

    Add signature constructor methods

--- crates/primitives/src/signature.rs
+++ crates/primitives/src/signature.rs
@@ -1,7 +1,26 @@
+use std::array::TryFromSliceError;
+
 #[derive(Debug)]
 pub struct Signature([u8; 65]);
 
 impl Signature {
+    pub fn new(full: [u8; 65]) -> Self {
+        Self(full)
+    }
+
+    pub fn try_from_slice(slice: &[u8]) -> Result<Self, TryFromSliceError> {
+        Ok(Self(slice.try_into()?))
+    }
+
+    pub fn from_rsv(r: &[u8; 32], s: &[u8; 32], v: u8) -> Self {
+        let mut full = [0u8; 65];
+        full[0..32].copy_from_slice(r);
+        full[32..64].copy_from_slice(s);
+        full[64] = v;
+
+        Self(full)
+    }
+
     pub fn full(&self) -> &[u8; 65] {
         &self.0
     }
