7681e56c6933a2e91aef705e98c89a63580ce4c5
yaziciahmet <yaziciahmet991@gmail.com>
2024-11-15 21:57:38 +0700

    TransactionPool struct

--- crates/txpool/src/txpool.rs
+++ crates/txpool/src/txpool.rs
@@ -1,4 +1,19 @@
-pub struct TransactionPool {}
+use crate::{pending::PendingPool, PoolTransaction};
+
+pub struct TransactionPool {
+    pending: PendingPool,
+}
+
+impl TransactionPool {
+    pub fn add_transaction(&mut self, transaction: PoolTransaction) {
+        // TODO: verify tx
+
+        // let account = self.storage.get_account(transaction.from)
+        // if account.nonce == transaction.nonce {
+        //   self.pending.add_transaction()
+        // }
+    }
+}
 
 // 1. add_transaction()
 // 2. validate tx -> validate signature
