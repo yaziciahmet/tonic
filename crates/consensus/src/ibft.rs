118ddf8667a10a2f3754591b62561d827eb3e2b1
yaziciahmet <yaziciahmet991@gmail.com>
2024-12-12 11:43:24 +0100

    Fmt

--- crates/consensus/src/ibft.rs
+++ crates/consensus/src/ibft.rs
@@ -144,7 +144,8 @@ where
             // Add the proposal to messages and broadcast it to peers
             self.messages.add_proposal_message(proposal.clone()).await;
             self.broadcast
-                .broadcast(IBFTMessage::Proposal(proposal.clone())).await?;
+                .broadcast(IBFTMessage::Proposal(proposal.clone()))
+                .await?;
 
             proposal
         } else {
@@ -193,7 +194,9 @@ where
         self.messages
             .add_prepare_message(prepare.clone(), self.signer.address())
             .await;
-        self.broadcast.broadcast(IBFTMessage::Prepare(prepare)).await?;
+        self.broadcast
+            .broadcast(IBFTMessage::Prepare(prepare))
+            .await?;
 
         Ok(())
     }
